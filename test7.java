7cb4b716246e20953336a8319dba81a1979f3679
challapavan123 <125641745+challapavan123@users.noreply.github.com>
2023-02-27 09:37:14 +0530

    Create test7.java

---
+++ test7.java
@@ -0,0 +1,21 @@
+//program on checked exception (not serious)
+//ex (ArithmethmeticExpression, null point, Arrayindexoutofbounds)
+//for checking exception try-catch is mandatory
+//throws is an alternate solution for try-catch
+//ignoring exception
+package classobjectdemo;
+
+public class test7 {
+
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("Program begins");
+		//throw new ArithmeticException("Division by zero");
+		throw new InterruptedException("Thread Interruption");
+		}
+		
+		}
+
+	
+
+
