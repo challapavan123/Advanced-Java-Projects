f12ad0a355a15139bb2eacb23169d58912e5de7e
challapavan123 <125641745+challapavan123@users.noreply.github.com>
2023-02-27 09:34:21 +0530

    Create test6.java
    done

---
+++ test6.java
@@ -0,0 +1,17 @@
+//program on unchecked exception (not serious)
+//ex (ArithmethmeticExpression, null point, Arrayindexoutofbounds)
+package classobjectdemo;
+
+public class test6 {
+
+	public static void main(String[] args) {
+		System.out.println("program begins");
+		int x=5;
+		int y=0;
+		int z=x/y;   //run time mistake
+	    System.out.println("end of the program");
+		
+
+	}
+
+}
