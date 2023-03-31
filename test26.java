364204140be8c4ade25c4c6b7550847115f0ada4
challapavan123 <125641745+challapavan123@users.noreply.github.com>
2023-03-31 01:09:33 -0700

    Update test26.java
    done

--- test26.java
+++ test26.java
@@ -1,20 +1,25 @@
 //use of channel and and buffer to write a file
 import java.io.RandomAccessFile;
 import java.nio.*;
+import java.nio.channels.FileChannel;
 public class test26 {
 
 	public static void main(String[] args) throws Exception{
-		String data="mobile-iphoe 14, price-80k, okia 2600, price-2600";
+		String data="mobile-iphone 14, price-80k,  nokia 2600, price-2600";
 		RandomAccessFile file = new RandomAccessFile("C:\\pavan\\amazon.store","rw");
 		
 		ByteBuffer buffer = ByteBuffer.allocate(data.length());
-		System.out.println("contents of buffer: "+buffer.getChar(2));
+		
 		for (int i = 0; i < data.length(); i++) {
 			buffer.put((byte) (data.charAt(i)));
 		}
-		for (int i = 0; i < data.length(); i++) {
-		System.out.println("contents of buffer: "+(char)buffer.get(i));
 
-	}
+	
+        FileChannel channel=file.getChannel(); 
+        buffer.rewind();
+		channel.write(buffer);
+		System.out.println("Data written to file");
+		channel.close();
+		file.close();
 
 }}
